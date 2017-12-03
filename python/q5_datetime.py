6c62d2b0f046e05369489110e52fcc3bb9a2153f
Emy <emy.pararita@gmail.com>
2017-12-02 16:28:05 -0800

    05a Python

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,42 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+from datetime import \
+  datetime, timedelta
+import locale
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+def td(d_start, d_stop, fmt, lc_time=None):
+  """Return timedelta for d_start..d_end using fmt format
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+     If date names are involved (month, day of week) set LC_TIME
+     locale as needed.
+
+  """
+
+  if lc_time:
+    lc_time_sav = locale.getlocale(locale.LC_TIME)
+    locale.setlocale(locale.LC_TIME, lc_time)
+  delta = datetime.strptime(d_stop, fmt) \
+          - datetime.strptime(d_start, fmt)
+  if lc_time:
+    locale.setlocale(locale.LC_TIME, lc_time_sav)
+  return delta
+
+
+####a)
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
+print("a) num_days = {}".format(td(date_start, date_stop,
+                                   '%m-%d-%Y').days))
+
+####b)
+date_start = '12312013'
+date_stop = '05282015'
+print("b) num_days = {}".format(td(date_start, date_stop,
+                                   '%m%d%Y').days))
+
+####c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+print("c) num_days = {}".format(td(date_start, date_stop,
+                                   '%d-%b-%Y', 'en_US.utf8').days))
+
